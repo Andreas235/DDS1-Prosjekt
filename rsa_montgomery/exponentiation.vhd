06a3e9c2ab3c934eed5ee0d3e7c5357b1834ed4c
Andreas Teodor Nysæter <andreas.nysater@ntnu.no>
2025-10-15 16:19:44 +0200

    Lagt til noen filer

---
+++ rsa_montgomery/exponentiation.vhd
@@ -0,0 +1,41 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity exponentiation is
+	generic (
+		C_block_size : integer := 256
+	);
+	port (
+		--input controll
+		valid_in	: in STD_LOGIC;
+		ready_in	: out STD_LOGIC;
+
+		--input data
+		message 	: in STD_LOGIC_VECTOR ( C_block_size-1 downto 0 );
+		key 		: in STD_LOGIC_VECTOR ( C_block_size-1 downto 0 );
+        r2_mod_n    : in std_logic_vector(C_BLOCK_SIZE-1 downto 0);  -- Femboy
+        n0_inv      : in std_logic_vector(31 downto 0);              -- Femboy
+
+		--ouput controll
+		ready_out	: in STD_LOGIC;
+		valid_out	: out STD_LOGIC;
+
+		--output data
+		result 		: out STD_LOGIC_VECTOR(C_block_size-1 downto 0);
+
+		--modulus
+		modulus 	: in STD_LOGIC_VECTOR(C_block_size-1 downto 0);
+
+		--utility
+		clk 		: in STD_LOGIC;
+		reset_n 	: in STD_LOGIC
+	);
+end exponentiation;
+
+
+architecture expBehave of exponentiation is
+begin
+	result <= message xor modulus;
+	ready_in <= ready_out;
+	valid_out <= valid_in;
+end expBehave;
